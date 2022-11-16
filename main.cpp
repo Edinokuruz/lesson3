b8203f4e202a8abb71ec65e7b2fb38635d0bb974
Edinokuruz <brusnik87@mail.ru>
2022-11-16 12:24:13 +1000

    change 5

--- main.cpp
+++ main.cpp
@@ -6,5 +6,8 @@ int main()
     std::cout << "Hello, World!" << std::endl;
     std::cout << "How are you!" << std::endl;
     std::cout << "I'm OK!" << std::endl;
+    std::cout << "And you?" << std::endl;
+    int par;
+    std::cin >> par;
     return 0;
 }
